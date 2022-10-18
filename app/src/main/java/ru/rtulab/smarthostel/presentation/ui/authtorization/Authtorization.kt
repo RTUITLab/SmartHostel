f59fdc84961437dd2ec5d4ee10872eaded7c0ba8
Alexander <mrapple100@yandex.ru>
2022-10-18 22:34:37 +0300

    OutlinedDropDown

--- app/src/main/java/ru/rtulab/smarthostel/presentation/ui/authtorization/Authtorization.kt
+++
@@ -1,2 +0,0 @@
-package ru.rtulab.smarthostel.presentation.ui.authtorization
-
--- app/src/main/java/ru/rtulab/smarthostel/presentation/ui/authtorization/components/DropDown.kt
+++ app/src/main/java/ru/rtulab/smarthostel/presentation/ui/authtorization/components/DropDown.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
@@ -17,22 +18,25 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
 
 @Preview
 @Composable
-fun DownDrop(){
+fun DropDown(
+    label:String="label",
+    array:List<String> = listOf("Delhi", "Mumbai", "Chennai", "Kolkata", "Hyderabad", "Bengaluru", "Pune")
+){
 
     // Declaring a boolean value to store
     // the expanded state of the Text Field
     var mExpanded = remember { mutableStateOf(false) }
 
     // Create a list of cities
-    val mCities = listOf("Delhi", "Mumbai", "Chennai", "Kolkata", "Hyderabad", "Bengaluru", "Pune")
 
     // Create a string value to store the selected city
-    var mSelectedText = remember { mutableStateOf("") }
+    var mSelectedText = remember { mutableStateOf(array[0] ?:"") }
 
     var mTextFieldSize = remember { mutableStateOf(Size.Zero)}
 
@@ -57,11 +61,12 @@ fun DownDrop(){
                     // the DropDown the same width
                     mTextFieldSize.value = coordinates.size.toSize()
                 },
-            label = { Text("Label") },
+            label = { Text(text = label) },
             trailingIcon = {
                 Icon(icon,"contentDescription",
                     Modifier.clickable { mExpanded.value = !mExpanded.value })
-            }
+            },
+            shape = RoundedCornerShape(8.dp)
         )
 
         // Create a drop-down menu with list of cities,
@@ -72,7 +77,7 @@ fun DownDrop(){
             modifier = Modifier
                 .width(with(LocalDensity.current){mTextFieldSize.value.width.toDp()})
         ) {
-            mCities.forEach { label ->
+            array.forEach { label ->
                 DropdownMenuItem(onClick = {
                     mSelectedText.value = label
                     mExpanded.value = false
