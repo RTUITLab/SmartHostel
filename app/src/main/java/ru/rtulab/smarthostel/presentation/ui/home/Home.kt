90c440851f3f6562006825c4e95c0189e0ca5190
Alexander <mrapple100@yandex.ru>
2022-10-19 22:21:59 +0300

    HomeUI prepare

--- app/src/main/java/ru/rtulab/smarthostel/presentation/ui/home/Home.kt
+++ app/src/main/java/ru/rtulab/smarthostel/presentation/ui/home/Home.kt
@@ -1,8 +1,36 @@
 package ru.rtulab.smarthostel.presentation.ui.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ru.rtulab.smarthostel.R
+import ru.rtulab.smarthostel.presentation.ui.common.ObjectCardWithDate
 
 @Composable
 fun Home(){
+    //val array = listOf<>()
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(vertical = 16.dp),
+            text = stringResource(R.string.LastBooking),
+            fontSize = 22.sp,
+            lineHeight = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+        //LastBooking
+        ObjectCardWithDate()
 
+
+
+    }
 }
--- app/src/main/java/ru/rtulab/smarthostel/ui/theme/Color.kt
+++ app/src/main/java/ru/rtulab/smarthostel/ui/theme/Color.kt
@@ -9,6 +9,8 @@ val Teal200 = Color(0xFF03DAC5)
 
 val Background = Color(0xFF1E1E1E)
 val Accent = Color(0xFF625EFF)
+val Accent50 = Color(0xBF625EFF)
+
 val White = Color(0xFFF2F2F2)
 val Secondary = Color(0xFF282828)
 val Green = Color(0xFF4EBD3C)
