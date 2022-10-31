afabe40d59a6ef32acf5cb8afb7e537497a52392
Alexander <mrapple100@yandex.ru>
2022-10-31 23:48:42 +0300

    GetOne and Cancel API

--- app/src/main/java/ru/rtulab/smarthostel/data/remote/api/booking/BookingApi.kt
+++ app/src/main/java/ru/rtulab/smarthostel/data/remote/api/booking/BookingApi.kt
@@ -1,9 +1,7 @@
 package ru.rtulab.smarthostel.data.remote.api.booking
 
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 import ru.rtulab.smarthostel.data.remote.api.booking.models.BookingDto
 import ru.rtulab.smarthostel.data.remote.api.booking.models.RequestBookingCreate
 import ru.rtulab.smarthostel.data.remote.api.objects.ObjectWithDate
@@ -14,8 +12,18 @@ interface BookingApi {
     @GET("/reservations/")
     suspend fun getAll():List<BookingDto>
 
+    @GET("/reservations/{bookingId}")
+    suspend fun getOne(
+        @Path("bookingId") bookingId:String
+    ):BookingDto
+
     @POST("/reservations/")
     suspend fun createBook(
         @Body booking:RequestBookingCreate
+    ):Response<Unit>
+
+    @DELETE("/reservations/{bookingId}/cancel")
+    suspend fun cancel(
+        @Path("bookingId") bookingId: String
     ):Response<Unit>
 }
