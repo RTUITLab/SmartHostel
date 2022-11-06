e461c22e6ffaf99e43596e2b42cf5021252093f8
Alexander <mrapple100@yandex.ru>
2022-11-07 00:44:59 +0300

    Drawer Main

--- app/src/main/java/ru/rtulab/smarthostel/presentation/SmartHostel.kt
+++ app/src/main/java/ru/rtulab/smarthostel/presentation/SmartHostel.kt
@@ -4,10 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.res.painterResource
@@ -16,6 +13,8 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.google.accompanist.pager.ExperimentalPagerApi
+import kotlinx.coroutines.launch
 import ru.rtulab.smarthostel.presentation.navigation.AppScreen
 import ru.rtulab.smarthostel.presentation.navigation.LocalNavController
 import ru.rtulab.smarthostel.presentation.navigation.NavigationGraph
@@ -24,6 +23,8 @@ import ru.rtulab.smarthostel.presentation.ui.common.BottomSheet
 import ru.rtulab.smarthostel.presentation.ui.common.header.BasicTopAppBar
 import ru.rtulab.smarthostel.presentation.ui.common.bottomsheet.BottomSheetViewModel
 import ru.rtulab.smarthostel.presentation.ui.common.burgermenu.BurgerMenuViewModel
+import ru.rtulab.smarthostel.presentation.ui.common.drawer.Drawer
+import ru.rtulab.smarthostel.presentation.ui.common.drawer.DrawerItem
 import ru.rtulab.smarthostel.presentation.ui.common.sharedElements.LocalSharedElementsRootScope
 import ru.rtulab.smarthostel.presentation.ui.common.sharedElements.SharedElementsRoot
 import ru.rtulab.smarthostel.presentation.ui.common.topAppBar.AppBarViewModel
@@ -33,6 +34,7 @@ import ru.rtulab.smarthostel.ui.theme.Accent
 import ru.rtulab.smarthostel.ui.theme.White
 import ru.rtulab.smarthostel.ui.theme.White50
 
+@ExperimentalPagerApi
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SmartHostel(
@@ -47,6 +49,8 @@ fun SmartHostel(
     val navController = LocalNavController.current
 
     var sharedElementScope = LocalSharedElementsRootScope.current
+
+    val scope = rememberCoroutineScope()
 
     val onBackAction: () -> Unit = {
         if (sharedElementScope?.isRunningTransition == false)
@@ -63,6 +67,11 @@ fun SmartHostel(
     }
 
     val tabs = appTabsViewModel.appTabs.collectAsState().value
+    val fourTabs = appTabsViewModel.appFourTabs.collectAsState().value
+
+    val drawertabs = appTabsViewModel.appTabs.collectAsState().value
+
+    val scaffoldState = rememberScaffoldState()
 
     ModalBottomSheetLayout(
         sheetState = bottomSheetViewModel.bottomSheetState,//заменить
@@ -78,22 +87,79 @@ fun SmartHostel(
     ) {
         Scaffold(
             modifier = if(bottomSheetViewModel.visibilityAsState.collectAsState().value) Modifier.blur(50.dp) else Modifier.blur(0.dp),
+            scaffoldState = scaffoldState,
             drawerContent = {
+                val currentTab by appBarViewModel.currentTab.collectAsState()
 
+                Drawer(
+                    backgroundColor = Accent,
+                    contentColor = White
+                ) {
+                    tabs.forEach { tab ->
+
+                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentDestination = navBackStackEntry?.destination
+
+                        DrawerItem(
+                            tab = tab,
+                            scope = scope,
+                            selectedContentColor = White50,
+                            unselectedContentColor = Accent,
+                            selected = currentDestination?.hierarchy?.any { it.route == tab.route } == true,
+                            onItemClick = {
+                                scope.launch {
+                                    scaffoldState.drawerState.close()
+                                }
+                                // As per https://stackoverflow.com/questions/71789903/does-navoptionsbuilder-launchsingletop-work-with-nested-navigation-graphs-in-jet,
+
+                                // it seems to not be possible to have all three of multiple back stacks, resetting tabs and single top behavior at once by the means
+                                // of Jetpack Navigation APIs, but only two of the above.
+                                // This code provides resetting and singleTop behavior for the default tab.
+                                if (tab == currentTab) {
+                                    navController.popBackStack(
+                                        route = tab.startDestination,
+                                        inclusive = false
+                                    )
+                                    return@DrawerItem
+                                }
+                                // This code always leaves default tab's start destination on the bottom of the stack. Workaround needed?
+                                navController.navigate(tab.route) {
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                    launchSingleTop = true
+
+                                    // We want to reset the graph if it is clicked while already selected
+                                    restoreState = tab != currentTab
+                                }
+                                appBarViewModel.setCurrentTab(tab)
+                            }
+                        )
+                    }
+                }
             },
             topBar ={
                 when (currentScreen) {
                     AppScreen.BookingCreate -> BasicTopAppBar(
+                        scope = scope,
+                        drawerState = scaffoldState.drawerState,
                         backgroundColor = Accent,
                         text = stringResource(currentScreen.screenNameResource),
                         //onBackAction = onBackAction
                     )
                     AppScreen.BookingCreateSecond -> BasicTopAppBar(
+                        scope = scope,
+
+                        drawerState = scaffoldState.drawerState,
+
                         backgroundColor = Accent,
                         text = stringResource(currentScreen.screenNameResource),
                         //onBackAction = onBackAction
                     )
                     else -> BasicTopAppBar(
+                        scope = scope,
+
+                        drawerState = scaffoldState.drawerState,
                         text = stringResource(currentScreen.screenNameResource),
                         //onBackAction = onBackAction
                     )
@@ -121,7 +187,7 @@ fun SmartHostel(
                     backgroundColor = Accent,
                     contentColor = White
                 ) {
-                    tabs.forEach { tab ->
+                    fourTabs.forEach { tab ->
 
                         val navBackStackEntry by navController.currentBackStackEntryAsState()
                         val currentDestination = navBackStackEntry?.destination
